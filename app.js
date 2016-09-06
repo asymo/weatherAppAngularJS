9fcd56f64a7089b85e8cd84b686b5749a920b254
Christopher J Symons <cjsymons86@gmail.com>
2016-09-06 18:27:48 +0100

    updated pages

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ weatherApp.config(function($routeProvider) {
 	.when('/forecast', {
 		templateUrl: 'pages/forecast.htm',
 		controller: 'forecastController'
+	})
+    
+    .when('/forecast/:days', {
+		templateUrl: 'pages/forecast.htm',
+		controller: 'forecastController'
 	});
 });
 
@@ -28,11 +33,28 @@ weatherApp.service('cityService', function(){
 weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
 	
 	$scope.city = cityService.city;
+    
+    $scope.$watch('city', function(){
+         cityService.city = $scope.city;
+    });
 	
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService){
 	
 	$scope.city = cityService.city;
+    $scope.days = $routeParams.days || 2;
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=79a15aafd0aa51d711d993175adc25fd", {callback: "JSON_CALLBACK"}, { get: {method: "JSONP"}});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+    
+    $scope.convertToFahrenheit = function(degK) {
+        return Math.round((1.8 * (degK - 273)) + 32);
+    };
+    
+    $scope.convertToDate = function(dt) {
+        return new Date(dt * 1000);
+    };
 	
 }]);
